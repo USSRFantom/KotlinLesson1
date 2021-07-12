a9bed9c47af6fbfc224f941ebe6b8946c24c8af6
UssrFantom <dimkakrohin@mail.ru>
2021-07-12 21:51:57 +0300

    Hello Kotlin!

--- app/src/main/java/space/ussrfantom/kotlinlesson1/MainActivity.kt
+++ app/src/main/java/space/ussrfantom/kotlinlesson1/MainActivity.kt
@@ -2,11 +2,15 @@ package space.ussrfantom.kotlinlesson1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        helloWorld()
     }
-    
+    fun helloWorld(){
+       Log.i("HelloWorld", "Hello Kotlin!")
+    }
 }
